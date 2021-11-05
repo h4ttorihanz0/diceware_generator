14a6a0a7a67dbb7069b5200b3e03ab202276cc26
h4ttorihanz0 <49950584+h4ttorihanz0@users.noreply.github.com>
2021-11-05 08:56:58 -0500

    Update diceware_gen.py

--- diceware_gen.py
+++ diceware_gen.py
@@ -1,11 +1,11 @@
 import random
 
 def get_num():
-    x = random.randrange(1, 6)
-    x1 = random.randrange(1, 6)
-    x2 = random.randrange(1, 6)
-    x3 = random.randrange(1, 6)
-    x4 = random.randrange(1, 6)
+    x = random.randrange(1, 7)
+    x1 = random.randrange(1, 7)
+    x2 = random.randrange(1, 7)
+    x3 = random.randrange(1, 7)
+    x4 = random.randrange(1, 7)
 
     print(x,x1,x2,x3,x4)
 
