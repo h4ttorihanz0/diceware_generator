84fa77986577e4718e7b43080827ca4837b454f8
h4ttorihanz0 <49950584+h4ttorihanz0@users.noreply.github.com>
2021-11-04 20:25:05 -0500

    Add files via upload

---
+++ diceware_gen.py
@@ -0,0 +1,12 @@
+import random
+
+def get_num():
+    x = random.randrange(1, 6)
+    x1 = random.randrange(1, 6)
+    x2 = random.randrange(1, 6)
+    x3 = random.randrange(1, 6)
+    x4 = random.randrange(1, 6)
+
+    print(x,x1,x2,x3,x4)
+
+get_num()
